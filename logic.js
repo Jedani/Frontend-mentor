0729116cde0672b10255688b8bef0ca09bcd00b4
unknown <allansichangi35@gmail.com>
2021-11-03 12:33:51 +0300

    added the reset functionality

--- logic.js
+++ logic.js
@@ -1,3 +1,17 @@
+let billInput = document.querySelector("#iconone");
+let numberOfPeople = document.querySelector("#icontwo");
+let tipAmount = document.querySelector("#tip");
+let total = document.querySelector("#total");
+
+let num = "<span> 0.00</span>";
+
+function tipAmountPerPerson() {
+	console.log(billInp);
+}
+
+function totalPerPerson() {}
+
 function reset() {
-	alert("clicked reset");
+	tipAmount.innerHTML = num;
+	total.innerHTML = num;
 }
