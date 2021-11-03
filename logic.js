ab414254f86d94ed90a33e18c6f148a9e1515c73
unknown <allansichangi35@gmail.com>
2021-11-04 02:25:09 +0300

    new inputs

--- logic.js
+++ logic.js
@@ -1,17 +1,4 @@
-let billInput = document.querySelector("#iconone");
-let numberOfPeople = document.querySelector("#icontwo");
-let tipAmount = document.querySelector("#tip");
-let total = document.querySelector("#total");
-
-let num = "<span> 0.00</span>";
-
-function tipAmountPerPerson() {
-	console.log(billInp);
-}
-
-function totalPerPerson() {}
-
-function reset() {
-	tipAmount.innerHTML = num;
-	total.innerHTML = num;
-}
+function bill() {}
+function custom() {}
+function people() {}
+function reset() {}
