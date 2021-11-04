492337518ee2abaa2762831fc62e591886a6418f
unknown <allansichangi35@gmail.com>
2021-11-04 04:34:08 +0300

    modified functionality

--- logic.js
+++ logic.js
@@ -1,4 +1,27 @@
-function bill() {}
-function custom() {}
-function people() {}
-function reset() {}
+let reseting =
+	"<span id=reset><span id=ones>0</span><span>.</span><span id=cents>00</span></span>";
+
+document.getElementById("iconone").addEventListener("input", bill);
+
+document.getElementById("icontwo").addEventListener("input", people);
+
+document.getElementById("customs").addEventListener("input", custom);
+
+function bill() {
+	alert("clicked");
+}
+function custom() {
+	alert("customs");
+}
+
+function people() {
+	alert("people");
+}
+
+function reset() {
+	const value = document.getElementById("reset");
+	value.innerHTML = reseting;
+	document.getElementById("iconone").value = "";
+	document.getElementById("icontwo").value = "";
+	document.getElementById("customs").value = "";
+}
